1fd75c4b1ba4f16a7b93ffce219aaa84421ccf4c
Yassin Makhloufi <yassin@studi.se>
2018-06-08 16:12:27 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# crowdtask-spa
+
+> The VueJS font-end of the crowdtask-project (https://github.com/ymakhloufi/crowdtask)
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+    <div id="app">
+        <h1>{{ msg }}</h1>
+        <a>Test</a>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App',
+      }
+    },
+  }
+</script>
+
+<style lang="scss">
+    #app {
+        font-family: Tahoma, Arial, Helvetica, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        color: #aaa;
+        background-color: #666;
+    }
+
+    h1, h2 {
+        font-weight: normal;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #97f;
+    }
+</style>
